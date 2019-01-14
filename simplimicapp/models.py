9788951f3b2ed997a7e4b3de6e92b78c8f77ed85
Thore Buergel <thm_junk@t-online.de>
2019-01-14 10:36:50 +0000

    Added drug, diagnosis and measurement mocks :cyclone:

--- simplimicapp/models.py
+++ simplimicapp/models.py
@@ -43,22 +43,48 @@ class Admission(models.Model):
     # TODO: define the link from admission to patient
     #3
 
-class StaticDescriptor(models.Model):
+
+class Descriptor(models.Model):
     """
-    The class for a static descriptor. Should hold the value and name of descriptor.
+    The class for a descriptor.
+
+    This holds measurements. either a single if its static, or multiple if its a TS.
+    """
+    # TODO: find out if we need an explicit definition of the HADM and SUBJID here or if queries
+    #       and relations will take care of that
+
+    # TODO: check out how MEasurements can be linked to the descriptor
+    # 1 to many relation needed here
+
+
+class Diagnosis(models.Model):
+    """
+    Holds information on the diagnosis.
+    """
+    seq_num = models.IntegerField(default=None)     # e.g. the rank of the admission in the patient
+    icd_code = models.IntegerField(max_length=6)
+    icd_class = models.IntegerField(max_length=2)
+
+
+class Measurement(models.Model):
+    """
+    Holds information on a single measurement
     """
     descr_name = models.CharField(max_length=20)
-    value = models.IntegerField(default=0)
+    itemID = models.IntegerField(default=None)
+    chart_time = models.DateTimeField(default=None)
+    value = models.IntegerField(default=None)
+    unit = models.CharField(max_length=10)
 
     # TODO: define the link from Descriptor to admission
     # TODO: check whether values are `str` or `num`
 
-class TSDescriptor(models.Model):
-    """
-    The class for an time series descriptor.
-    
-    """
 
-
+class Drug(models.Model):
+    """
+    Holds information about a drug
+    """
+    # TODO: implement
+    pass
 
 
