27d9ed62fc626c99dd39a5e71f37ba5814ca586c
Thore Buergel <thm_junk@t-online.de>
2019-01-16 15:03:03 +0000

    Added full drug class and services mock class :hrose::fire:

--- simplimicapp/models.py
+++ simplimicapp/models.py
@@ -28,6 +28,7 @@ class Admission(models.Model):
     )
 
     # meta
+    subjectID = models.IntegerField(default=None)  # TODO: need to define explicitly or is implicitly through queries enough?
     admID = models.IntegerField(default=None)
     adm_time = models.DateTimeField(default=None)
     disch_time = models.DateTimeField(default=None)
@@ -44,6 +45,14 @@ class Admission(models.Model):
     #3
 
 
+class Sevice(models.Model):
+    """
+    Holds information on the servive
+    """
+    # TODO:  no real need to define this as a class, might also be an attribute?
+    pass
+
+
 class Descriptor(models.Model):
     """
     The class for a descriptor.
@@ -52,18 +61,12 @@ class Descriptor(models.Model):
     """
     # TODO: find out if we need an explicit definition of the HADM and SUBJID here or if queries
     #       and relations will take care of that
+    subjectID = models.IntegerField(default=None)  # TODO: need to define explicitly or is implicitly through queries enough?
+    admID = models.IntegerField(default=None)
+
 
     # TODO: check out how MEasurements can be linked to the descriptor
     # 1 to many relation needed here
-
-
-class Diagnosis(models.Model):
-    """
-    Holds information on the diagnosis.
-    """
-    seq_num = models.IntegerField(default=None)     # e.g. the rank of the admission in the patient
-    icd_code = models.IntegerField(max_length=6)
-    icd_class = models.IntegerField(max_length=2)
 
 
 class Measurement(models.Model):
@@ -73,18 +76,51 @@ class Measurement(models.Model):
     descr_name = models.CharField(max_length=20)
     itemID = models.IntegerField(default=None)
     chart_time = models.DateTimeField(default=None)
-    value = models.IntegerField(default=None)
+    value = models.FloatField(default=None)
     unit = models.CharField(max_length=10)
 
     # TODO: define the link from Descriptor to admission
     # TODO: check whether values are `str` or `num`
 
 
+class Diagnosis(models.Model):
+    """
+    Holds information on the diagnosis.
+    """
+    seq_num = models.IntegerField(default=None, max_length=1)     # e.g. the rank of the diagnosis in the end of the admission
+    icd_code = models.IntegerField(max_length=6)
+    icd_class = models.IntegerField(max_length=2)
+
+
 class Drug(models.Model):
     """
     Holds information about a drug
     """
-    # TODO: implement
-    pass
+    DRUG_TYPE_CHOICES = (
+        ('M', 'MAIN'),
+         ('A', 'ADDITIVE'),
+         ('B', 'BASE')
+    )
+    ROUTE_CHOICES = (
+        # TODO: implement
+    )
+    """
+    PROD_STRENGTH,DOSE_VAL_RX,DOSE_UNIT_RX,FORM_VAL_DISP,FORM_UNIT_DISP,ROUTE """
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    drug_type = models.CharField(choices=DRUG_TYPE_CHOICES, default=None)
+    drug = models.CharField(default=None, max_length=25)
+    drug_name_poe = models.CharField(default=None, max_length=25)
+    drug_name_generic = models.CharField(default=None, max_length=25)
+    formulary_drug_cd = models.CharField(default=None, max_length=15)
+    gsn = models.IntegerField(default=None, max_length=15)
+    ndc = models.FloatField(default=None, max_length=25)
+    prod_strength = models.CharField(default=None, max_length=25)
+    dose_val_rx = models.CharField(default=None, max_length=25)  # can't take  float here as there are ranges somtimes
+    dose_unit_rx = models.CharField(default=None, max_length=25)
+    form_val_disp = models.CharField(default=None, max_length=25)  # can't take  float here as there are ranges somtimes
+    form_unit_disp = models.CharField(default=None, max_length=25)
+    route = models.CharField(default=None, max_length=25)   # TODO: establish a CHOICE set here that is hierarchical!
+
 
 
