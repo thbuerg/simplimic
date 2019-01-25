c2150bf23eb12e29a72eea89587e385fbe967153
Thore Buergel <thm_junk@t-online.de>
2019-01-25 14:30:01 +0000

    Added ICUstay model, rewrote the others, untested :rotating_light::rotating_light:

--- simplimicapp/models.py
+++ simplimicapp/models.py
@@ -4,18 +4,26 @@ from django.db import models
 class Patient(models.Model):
     """
     The patient class for MIMIC-III.
+
+    we keeo all  fields except row-IDs.
     """
+    # TODO: 1. Determine if the pre-pop-processing will:
+    # TODO:     - integerize static patients vars (GENDER, ETHNICITY)
     subjectID = models.IntegerField(default=None, primary_key=True)
-    gender = models.CharField(max_length=1)
+    gender = models.CharField(max_length=10)
     age = models.IntegerField(default=0)
     date_of_birth = models.DateField(default=None)
+    date_of_death = models.DateField(default=None)
+    date_of_death_hosp = models.DateField(default=None)
+    date_of_death_ssn = models.DateField(default=None)   # ssn = stationary?
+    expire_flag = models.BooleanField(default=None)   # TODO: is there a default?
 
 
 class Admission(models.Model):
     """
     Holds the information for a single admission period.
     """
-    ADMISSION_CHOICES = (
+    ADMISSION_CHOICES = (    # TODO: since we're storing real data now: do we still want these?
         ('Elective',
          (('NB', 'newborn'),
          ('EL', 'elective')
@@ -31,14 +39,47 @@ class Admission(models.Model):
     admID = models.IntegerField(default=None,  primary_key=True)
     adm_time = models.DateTimeField(default=None)
     disch_time = models.DateTimeField(default=None)
+    death_time = models.DateTimeField(default=None)
     adm_type = models.CharField(choices=ADMISSION_CHOICES, default=None, max_length=2)
+    adm_location = models.CharField(default=None, max_length=40)  # TODO: get choices for this?
+    disch_location = models.CharField(default=None, max_length=40)  # TODO: get choices for this?
+    insurance = models.CharField(default=None, max_length=15)  # TODO: get choices for this?
+    language = models.CharField(default=None, max_length=10)  # may be `nan` (still str)
+    religion = models.CharField(default=None, max_length=10)  # Fun Experiment proposal: proof indifference between religions by demonstrating statistical insignificance of religion choice as model covariate (after stripping outgroups. Sorry my `7TH DAY ADVENTIST`s)
+    marital_status = models.CharField(default=None, max_length=10)  # -> GREAT EXPERIMENTS IMAGINABLE HERE....
+    ethnicity = models.CharField(default=None, max_length=50)
+    edregtime = models.DateTimeField(default=None)
+    edouttime = models.DateTimeField(default=None)
+    diagnosis = models.CharField(default=None, max_length=100)  # some of them are really detailed in description -> definitively better to use codes
+    hosp_exp_flag = models.BooleanField(default=None) # TODO find out what this is
+    has_chartevents = models.BooleanField(default=None)
 
+    # TODO:  these will no longer be part of the admission and will  have to be inferred over the subject.
     # Outcomes
     inpmor = models.BooleanField(default=None)  # in-hospital death
     pdismor = models.BooleanField(default=None)  # past discharge death (within 30 days)
     read = models.BooleanField(default=None)  # readmission (within 30)
     los = models.IntegerField(default=0)  # length of stay (in days)
     plos = models.BooleanField(default=None)  # prolonged length of stay
+
+
+class ICUstay(models.Model):
+    """
+    The class holding ICU stays:
+   "ROW_ID","SUBJECT_ID","HADM_ID","ICUSTAY_ID","DBSOURCE","FIRST_CAREUNIT","LAST_CAREUNIT", \
+   "FIRST_WARDID","LAST_WARDID","INTIME","OUTTIME","LOS"
+    """
+    subject = models.ForeignKey('Patient', on_delete=models.CASCADE)
+    admission = models.ForeignKey('Admission', on_delete=models.CASCADE)
+    icustayID = models.IntegerField(default=None,  primary_key=True)
+    db_source = models.CharField(default=None,  max_length=10) # TODO we most vertainly want a choice here
+    first_cu = models.CharField(default=None,  max_length=10)
+    last_cu = models.CharField(default=None,  max_length=10)
+    first_ward_id = models.IntegerField(default=None)
+    last_ward_id = models.IntegerField(default=None)
+    intime = models.DateTimeField(default=None)  # important field.
+    outtime = models.DateTimeField(default=None)  # important field.
+    los = models.IntegerField(default=None)
 
 
 class DescriptorValue(models.Model):
@@ -49,39 +90,65 @@ class DescriptorValue(models.Model):
     value
     unit (valueuom)
     """
+    KINDS = (
+        ('L', 'LABEVENT'),
+        ('C', 'CHARTEVENT')
+    )
+
+
     # keys
     subject = models.ForeignKey('Patient', on_delete=models.CASCADE)
     admission = models.ForeignKey('Admission', on_delete=models.CASCADE)
+    icustay = models.ForeignKey('ICUstay', on_delete=models.CASCADE)
 
     # Fields:
     itemID = models.IntegerField(default=None)#, primary_key=True)   # TODO sure? it might actually  be smart to have squential keys...
     chart_time = models.DateTimeField(default=None)
+    store_time = models.DateTimeField(default=None, null=True, blank=True)
+    cgID = models.CharField(default=None, max_length=10, null=True, blank=True)
     value = models.CharField(default=None, max_length=10)
     unit = models.CharField(max_length=10)
-    flag = models.CharField(default=None, max_length=8, null=True, blank=True) # abnormal or normal
-    kind = models.CharField(max_length=1)  # either L -> lab or C -> chart
+    warning = models.BooleanField(default=None, null=True, blank=True)
+    error = models.BooleanField(default=None, null=True, blank=True)
+    resultstatus = models.CharField(default=None, max_length=50, null=True, blank=True)  # contained only nans the top 1 Mio rows
+    stopped = models.CharField(default=None, max_length=50, null=True, blank=True)  # contained only nans the top 1 Mio rows
+    flag = models.CharField(default=None, max_length=8, null=True, blank=True) # abnormal or normal for lab values
+    kind = models.CharField(max_length=1, choices=KINDS)  # either L -> lab or C -> chart
 
 
 class Sevice(models.Model):
     """
     Holds information on the servive
-    """
-    # TODO:  no real need to define this as a class, might also be an attribute?
-    pass
-
-
-class Diagnosis(models.Model):
-    """
-    Holds information on the diagnosis.
+    "SUBJECT_ID","HADM_ID","TRANSFERTIME","PREV_SERVICE","CURR_SERVICE"
     """
     # keys
     subject = models.ForeignKey('Patient', on_delete=models.CASCADE)
     admission = models.ForeignKey('Admission', on_delete=models.CASCADE)
 
+    # fields:
+    transfertime = models.DateTimeField(default=None)
+
+    # TODO: standardize through choices?
+    prev_service = models.CharField(default=None, max_length=10)
+    curr_service = models.CharField(default=None, max_length=10)
+
+
+class Diagnosis(models.Model):
+    """
+    Holds information on the diagnosis.
+    "ROW_ID","SUBJECT_ID","HADM_ID","SEQ_NUM","ICD9_CODE"
+    """
+    # keys
+    subject = models.ForeignKey('Patient', on_delete=models.CASCADE)
+    admission = models.ForeignKey('Admission', on_delete=models.CASCADE)
+    # no ICU here
+
     # fields
     seq_num = models.IntegerField(default=None)     # e.g. the rank of the diagnosis in the end of the admission
     icd_code = models.IntegerField(primary_key=True)
-    icd_class = models.IntegerField()
+
+    # TODO: map this in prepopproc?:
+    #icd_class = models.IntegerField()
 
 
 class Prescription(models.Model):
@@ -99,6 +166,7 @@ class Prescription(models.Model):
     # keys
     subject = models.ForeignKey('Patient', on_delete=models.CASCADE)
     admission = models.ForeignKey('Admission', on_delete=models.CASCADE)
+    icustay = models.ForeignKey('ICUstay', on_delete=models.CASCADE)
 
     # fields
     start_date = models.DateTimeField()
